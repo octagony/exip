c32a14c45a5a089c2e024d7c0286e462c93f2c7e
octagony <moctagony@gmail.com>
2023-09-20 21:54:19 +0300

    [FEAT] Setup basic skeleton layot

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,26 +5,24 @@ import IPSerivce from "~/services/ip.service";
 // Interfaces
 import { IpInfo } from "~/types/ip.interface";
 //Components
-import Loader from "./Loader/Loader";
 import Header from "./Header/Header";
-import { Button } from "@suid/material";
+import Loader from "./Loader/Loader";
 
 export default function App() {
-  const [ipInfo, setIpInfo] = createSignal<IpInfo>();
+	const [ipInfo, setIpInfo] = createSignal<IpInfo>();
 
-  createEffect(async () => {
-    const ip = await IPSerivce.getIPInfo().then((data) => data);
-    // setIpInfo(ip);
-  }, []);
+	createEffect(async () => {
+		const ip = await IPSerivce.getIPInfo().then(data => data);
+		// setIpInfo(ip);
+	}, []);
 
-  return (
-    <>
-      <Header />
-      <Show when={ipInfo()} fallback={() => <Loader />}>
-        <p>{ipInfo()?.ip}</p>
-        <p>{ipInfo()?.location.continent}</p>
-      </Show>
-      <Button variant="contained">Hello test</Button>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Show when={ipInfo()} fallback={() => <Loader />}>
+				<p>{ipInfo()?.ip}</p>
+				<p>{ipInfo()?.location.continent}</p>
+			</Show>
+		</>
+	);
 }
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,8 +2,26 @@ import { Grid, Skeleton } from "@suid/material";
 
 export default function Loader() {
 	return (
-		<Grid container justifyContent="center">
-			<Skeleton variant="rectangular" width={"80%"} height={490} />
+		<Grid my={4} mx={4}>
+			<Grid container justifyContent="center">
+				<Skeleton variant="rectangular" width={"80%"} height={490} />
+			</Grid>
+			<Grid container my={4} justifyContent="space-evenly" gap={4}>
+				<Grid item xs={12} md={4}>
+					<Skeleton variant="rectangular" width={"100%"} height={300} />
+				</Grid>
+				<Grid item xs={12} md={4}>
+					<Skeleton variant="rectangular" width={"100%"} height={300} />
+				</Grid>
+				<Grid item xs={12}>
+					<Skeleton
+						variant="rectangular"
+						width={"80%"}
+						height={300}
+						sx={{ marginInline: "auto" }}
+					/>
+				</Grid>
+			</Grid>
 		</Grid>
 	);
 }
