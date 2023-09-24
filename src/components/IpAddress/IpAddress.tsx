a4a6518c34bec5f062b5c788b75d35643e3942ef
octagony <moctagony@gmail.com>
2023-09-24 15:06:20 +0300

    [FEAT] Basic setup for IpAddress component

--- src/components/IpAddress/IpAddress.tsx
+++ src/components/IpAddress/IpAddress.tsx
@@ -3,13 +3,21 @@ import { Box, Typography } from "@suid/material";
 export default function IpAddress() {
 	return (
 		<Box
-			sx={{ display: "flex", width: "100%" }}
+			sx={{ display: "flex", maxWidth: "30%", bgcolor: "primary.main" }}
 			justifyContent="center"
 			flexDirection="column"
 			alignItems="center"
+			mt={4}
+			marginInline={"auto"}
+			borderRadius={3}
+			p={2}
 		>
-			<Typography variant="h1">Your API Address:</Typography>
-			<Typography variant="h1">46.216.172.38</Typography>
+			<Typography variant="h5" color="common.white">
+				Your API Address:
+			</Typography>
+			<Typography variant="h1" color="common.white">
+				46.216.172.38
+			</Typography>
 		</Box>
 	);
 }
